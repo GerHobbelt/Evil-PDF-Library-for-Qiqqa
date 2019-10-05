9a65e3a10eb4b384f1beade6b5dfee12dd3428a4
Ger Hobbelt <ger@hobbelt.com>
2019-10-05 18:43:31 +0200

    fixed comment typo

--- set-up-directory-junctions-to-qiqqa-base-dir.bat
+++ set-up-directory-junctions-to-qiqqa-base-dir.bat
@@ -8,6 +8,6 @@ mklink /j INTRANET-EVIL-PDF-SAMPLES-FOR-QIQQA           %~dp0\INTRANET-EVIL-PDF-
 mklink /j INTRANET_E57B9774-4712-430E-93E0-E67433F7DF07 %~dp0\INTRANET_E57B9774-4712-430E-93E0-E67433F7DF07
 
 rem when you run this for Windows Explorer by right-mouse --: Run As Administrator,
-rem you surely would like to ascertain that this thing did what's advertisd on the can.
+rem you surely would like to ascertain that this thing did what's advertised on the can.
 rem Hence wait for user keypress - after heshe's done reviewing the cmd output.
 pause
