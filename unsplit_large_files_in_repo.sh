a96a70d05be67d9265d3816e2ae64e72b1b641ac
Ger Hobbelt <ger@hobbelt.com>
2020-12-30 17:28:40 +0100

    write a couple of quick bash scripts to SPLIT large files (and then mark them as hidden/ignored by git!) and UNSPLIT them again when you need them as-is. The alternative is GIT LFS, which is rather costly for a basic test archive like this one with some large PDF files.

---
+++ unsplit_large_files_in_repo.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+# 
+# As Github LFS storage is limited to 500M for free accounts and 2GB for commercial non-enterprise, 
+# this is the way to archive larger binary files in a git repo when you have some.
+# 
+
+if test "$#" -eq 2; then
+	echo ""
+	echo ""
+    # execute subcommand CMD ARG
+    if test "$1" == "SPLIT"; then
+    	echo "split $2"
+    	echo ""
+    	if ! test -f "$2.7z.001" ; then
+	    	echo 7z a -v40m -mx1 -- "$2.7z" "$2"
+	    	7z a -v40m -mx1 -- "$2.7z" "$2"
+	    else
+	    	echo "7ZIP split archive already exists for '$2'"
+	    fi
+		node ./update_gitignore_for_large_files.js "$2"
+    elif test "$1" == "UNSPLIT"; then
+    	echo "UNsplit $2"
+    	echo ""
+    	d=$( dirname -- "$2" )
+    	pushd "$d" 											 > /dev/null 2>&1
+    	s=$( basename -- "$2" )
+    	t=$( basename -- "$2" ".7z.001" )
+    	if ! test -f "$t" ; then
+	    	echo 7z e -- "$s"
+	    	7z e -- "$s"
+	    else
+	    	echo "Large target file '$t' already exists in '$d/'"
+	    fi
+    	popd     											 > /dev/null 2>&1
+		node ./update_gitignore_for_large_files.js "$d/$t"
+    else
+    	echo "unknown subcommand."
+    fi
+else
+	find . -type f -iname '*.7z.001' -a ! -ipath '*.git/*' -print0 | xargs -0 -n 1 "$0" UNSPLIT
+fi
---
+++ update_gitignore_for_large_files.js
@@ -0,0 +1,22 @@
+
+const fs = require('fs');
+const path = require('path');
+
+const filepath = process.argv[2];
+
+let src = fs.readFileSync(".gitignore", "utf8");
+if (!src.includes(filepath)) {
+	const header = "# large file split/unsplit support: ignoring the large files.";
+	src = src.trim();
+	if (!src.includes(header)) {
+		src += `
+
+${header}
+
+		`;
+	}
+	src += '\n' + filepath + '\n';
+
+	fs.writeFileSync(".gitignore", src, "utf8");
+}
+
