83b862ea61f3e382ec6a4a1463b470fa4320fae6
Ger Hobbelt <ger@hobbelt.com>
2021-03-11 19:50:17 +0100

    update bulktest script generator to SORT the PDF file list. This hould improve git diff inspections on the generated TSV files.

--- mk_mupdf_mujstest_script.sh
+++ mk_mupdf_mujstest_script.sh
@@ -7,8 +7,12 @@ echo "scan directory tree and collect all pdf files..."
 echo > all_pdf_files.lst
 ##find . -iname '*.pdf' -a ! -ipath '*/__mujstest/*' -a ! -ipath '*/__unitest/*' 				>> all_pdf_files.lst
 ##find ../base -iname '*.pdf' -a ! -ipath '*/__mujstest/*' -a ! -ipath '*/__unitest/*' 		>> all_pdf_files.lst
-find . -iname '*.pdf' -a ! -ipath '*/__mujstest/*' -a ! -ipath '*/__unitest/*' 				>> all_pdf_files.lst
-find Sample-PDFs-for-format-testing/Unicode* -iname '*.pdf' 								> unicode_spec_pdf_files.lst
+find . -iname '*.pdf' -a ! -ipath '*/__bulktest/*' -a ! -ipath '*/__mujstest/*' -a ! -ipath '*/__unitest/*' 				>> all_pdf_files.lst
+# sort the list (for git diffing improvement)
+cat all_pdf_files.lst | sort | uniq > tmp
+mv tmp all_pdf_files.lst
+
+find Sample-PDFs-for-format-testing/Unicode* -iname '*.pdf' 								| sort > unicode_spec_pdf_files.lst
 
 echo "generate mujstest script..."
 node "$( basename "$0" '.sh' ).js"
