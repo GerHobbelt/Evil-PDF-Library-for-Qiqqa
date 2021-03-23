b661f175c0664012a84c667224a8ec16643a40e0
Ger Hobbelt <ger@hobbelt.com>
2021-03-24 00:30:01 +0100

    cleaned up prep script; we're using the new DirScanner tool from now on. That one can correctly detect hardlinked duplicates on NTFS and will produce a file listing only **unique** files to test, which will cut out the duplicates surplus in the very long running bulk tests. +++!

--- mk_mupdf_mujstest_script.sh
+++ mk_mupdf_mujstest_script.sh
@@ -4,25 +4,16 @@
 pushd $(dirname "$0")                                                    2> /dev/null  > /dev/null
 
 echo "scan directory tree and collect all pdf files..."
-echo > all_pdf_files.lst
-##find . -iname '*.pdf' -a ! -ipath '*/__mujstest/*' -a ! -ipath '*/__unitest/*' 				>> all_pdf_files.lst
-##find ../base -iname '*.pdf' -a ! -ipath '*/__mujstest/*' -a ! -ipath '*/__unitest/*' 		>> all_pdf_files.lst
-#find . -iname '*.pdf' -a ! -ipath '*/__bulktest/*' -a ! -ipath '*/__mujstest/*' -a ! -ipath '*/__unitest/*' 				>> all_pdf_files.lst
+#find . -iname '*.pdf' -a ! -ipath '*/__bulktest/*' -a ! -ipath '*/__mujstest/*' -a ! -ipath '*/__unitest/*' 				> all_pdf_files.lst
 # list only unique PDF files:
-./DirScanner.exe -s -c -r X "*.pdf" 				>> all_pdf_files.lst
-# sort the list (for git diffing improvement)
-cat all_pdf_files.lst | sort -R > tmp
-mv tmp all_pdf_files.lst
+# sort the list SEMI-randomly (for git diffing improvement)
+./DirScanner.exe -s -c -r X -o all_pdf_files.lst "*.pdf"
 
 find Sample-PDFs-for-format-testing/Unicode* -iname '*.pdf' 								| sort > unicode_spec_pdf_files.lst
 
-#if test -d /w/SopKonijn/ ; then
-#	./DirScanner.exe -s -c -r X "W:/SopKonijn/*.pdf" | sort >> all_pdf_files_on_drive_W.lst
-#fi
-if test -f all_pdf_files_on_drive_W.lst ; then
-	cat all_pdf_files_on_drive_W.lst | sort -R > tmp
-	mv tmp all_pdf_files_on_drive_W.lst
-fi	
+if test -d /w/SopKonijn/ ; then
+	./DirScanner.exe -s -c -r X -o all_pdf_files_on_drive_W.lst "W:/SopKonijn/*.pdf"
+fi
 
 #echo "generate mujstest script..."
 node "$( basename "$0" '.sh' ).js"
