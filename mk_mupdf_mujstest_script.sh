b5f63019850c441cd1ab170964895b1c894b6223
Ger Hobbelt <ger@hobbelt.com>
2022-09-04 13:31:37 +0200

    - updated logbook notes about findings in `bulktest` runs to date. - cleaned & synced the logfile a bit: it's mirrored (kinda) in our Obsidian-based dev/design notes for Qiqqa; see that repo) - augmented the test-fileset producing script to also give us the entire set of files in the new Q: 'repo test drive'. (Those PDFs generally are not part of the `evil-base` corpus as they include private ones and a raw ddump + recovered backups of my own Qiqqa libraries.
    TODO:
    - augment the test-fileset producing script to also give us the entire set of backup copies / recovered / dumped Qiqqa metadata database files, so we can test those with our new SQLite codebase.

--- mk_mupdf_mujstest_script.sh
+++ mk_mupdf_mujstest_script.sh
@@ -10,6 +10,8 @@ echo "scan directory tree and collect all pdf files..."
 # sort the list SEMI-randomly (for git diffing improvement)
 ./DirScanner.exe -s -c -r X -o all_pdf_files.lst "*.pdf"
 
+./DirScanner.exe -s -c -o all_pdf_files.RAW.lst "*.pdf"
+
 find Sample-PDFs-for-format-testing/Unicode* -iname '*.pdf' 								| sort > unicode_spec_pdf_files.lst
 
 find Sample-PDFs-for-format-testing/pdfdraw-text-extract-horrors-and-low-quality-files -iname '*.pdf' | sort > text_extract_spec_pdf_files.lst
@@ -20,6 +22,12 @@ fi
 
 if test -d /z/lib/tooling/qiqqa/MuPDF/tests/ ; then
 	./DirScanner.exe -s -c -r X -o mupdf_test_files_in_dev_drive_Z.lst "Z:/lib/tooling/qiqqa/MuPDF/tests/*.pdf"
+fi
+
+if test -d /y/ ; then
+	./DirScanner.exe -s -c -o all_pdf_files_on_test_drive.RAW.lst "Y:/*.pdf"
+
+	./DirScanner.exe -s -c -o -r X all_pdf_files_on_test_drive.lst "Y:/*.pdf"
 fi
 
 
